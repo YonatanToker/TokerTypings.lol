071043f5114a4e31497abb789b2bedb65750c8e4
NinjaDog1 <yonatantoker123@gmail.com>
2023-10-24 13:23:00 +0300

    second stats attempt

--- app/Statistics/page.tsx
+++ app/Statistics/page.tsx
@@ -2,53 +2,53 @@
 import React from "react";
 import { LineChart } from "@/Components";
 import { useState, useEffect } from "react";
-type resultsArrType = {
-  wpm: number;
-  accuracy: number;
-  correctWords: number;
-  wrongWords: number;
-  level: string;
-};
-type ChartData = {
-  labels: number[];
-  datasets: { label: string; data: number[] }[];
-};
+// type resultsArrType = {
+//   wpm: number;
+//   accuracy: number;
+//   correctWords: number;
+//   wrongWords: number;
+//   level: string;
+// };
+// type ChartData = {
+//   labels: number[];
+//   datasets: { label: string; data: number[] }[];
+// };
 const page = () => {
-  const options = {
-    scales: {
-      x: {
-        beginAtZero: true,
-      },
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
-  const [resultsArr, setResultsArr] = useState<resultsArrType[]>([]);
+  // const options = {
+  //   scales: {
+  //     x: {
+  //       beginAtZero: true,
+  //     },
+  //     y: {
+  //       beginAtZero: true,
+  //     },
+  //   },
+  // };
+  // const [resultsArr, setResultsArr] = useState<resultsArrType[]>([]);
 
-  const [chartData, setChartData] = useState<ChartData>({
-    labels: [],
-    datasets: [{ label: "words per minute", data: [] }],
-  });
+  // const [chartData, setChartData] = useState<ChartData>({
+  //   labels: [],
+  //   datasets: [{ label: "words per minute", data: [] }],
+  // });
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const storedArr: resultsArrType[] = JSON.parse(
-        localStorage.getItem("resultsArr") || "[]"
-      );
-      setResultsArr(storedArr);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     const storedArr: resultsArrType[] = JSON.parse(
+  //       localStorage.getItem("resultsArr") || "[]"
+  //     );
+  //     setResultsArr(storedArr);
 
-      setChartData({
-        labels: storedArr.map((_, index) => index + 1),
-        datasets: [
-          {
-            label: "words per minute",
-            data: storedArr.map((data: resultsArrType) => data.wpm),
-          },
-        ],
-      });
-    }
-  }, []);
+  //     setChartData({
+  //       labels: storedArr.map((_, index) => index + 1),
+  //       datasets: [
+  //         {
+  //           label: "words per minute",
+  //           data: storedArr.map((data: resultsArrType) => data.wpm),
+  //         },
+  //       ],
+  //     });
+  //   }
+  // }, []);
 
   return (
     <div className="flex w-full flex-col items-center">
